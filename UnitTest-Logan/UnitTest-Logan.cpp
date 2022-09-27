4d16d4846bd7599ec3768b08b4a27630655036c3
Logan Huston <shadows7590@gmail.com>
2022-09-27 17:25:40 -0600

    Added header files for all pieces.

---
+++ UnitTest-Logan/UnitTest-Logan.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLogan
+{
+	TEST_CLASS(UnitTestLogan)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ UnitTest-Logan/testGame.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace UnitTestLogan
+{
+	TEST_CLASS(testGame)
+	{
+	public:
+
+		TEST_METHOD(constructorTest)
+		{
+			// setup
+			// exercise
+			// verify
+		}	// teardown
+	};
+
+
+}
